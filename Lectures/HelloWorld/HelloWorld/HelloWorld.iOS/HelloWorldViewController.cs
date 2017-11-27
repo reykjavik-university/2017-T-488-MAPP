01f636b47f9de85501d1a11414c667d6586b2154
Hrafn Loftsson <hrafn@kolibri.is>
2017-11-27 11:34:02 +0000

    Adds a HelloWorld template solution

---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/HelloWorldViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+
+namespace HelloWorld.iOS
+{
+    public partial class HelloWorldViewController : UIViewController
+    {
+        public HelloWorldViewController() : base("HelloWorldViewController", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
