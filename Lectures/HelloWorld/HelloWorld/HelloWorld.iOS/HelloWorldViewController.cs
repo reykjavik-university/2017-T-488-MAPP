01c042deea47c37f3d890382c501fd357a52006f
Hrafn Loftsson <hrafn@kolibri.is>
2017-11-27 12:51:32 +0000

    Removes HellowWorldViewController.xib and adds constants

--- Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/HelloWorldViewController.cs
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/HelloWorldViewController.cs
@@ -4,20 +4,11 @@ using UIKit;
 
 namespace HelloWorld.iOS
 {
-    public partial class HelloWorldViewController : UIViewController
+    public class HelloWorldViewController : UIViewController
     {
         private const double StartX = 20;
-
-        public HelloWorldViewController() : base("HelloWorldViewController", null)
-        {
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-
-            // Release any cached data, images, etc that aren't in use.
-        }
+        private const double StartY = 80;
+        private const double Height = 50;
 
         public override void ViewDidLoad()
         {
@@ -27,7 +18,7 @@ namespace HelloWorld.iOS
 
             var promptLabel = new UILabel()
             {
-                Frame = new CGRect(StartX, 80, this.View.Bounds.Width, 50),
+                Frame = new CGRect(StartX, StartY, this.View.Bounds.Width-2*StartX, Height),
                 Text = "Enter your name: "
             };
 
@@ -35,21 +26,21 @@ namespace HelloWorld.iOS
 
             var nameField = new UITextField()
             {
-                Frame = new CGRect(StartX, 130, this.View.Bounds.Width, 50),
+                Frame = new CGRect(StartX, StartY+Height, this.View.Bounds.Width-2*StartX, Height),
                 BorderStyle = UITextBorderStyle.RoundedRect
             };
 
             this.View.AddSubview(nameField);
 
             var greetingButton = UIButton.FromType(UIButtonType.RoundedRect);
-            greetingButton.Frame = new CGRect(StartX, 180, this.View.Bounds.Width / 2, 50);
+            greetingButton.Frame = new CGRect(StartX, StartY+2*Height, this.View.Bounds.Width-2*StartX, Height);
             greetingButton.SetTitle("Greet me", UIControlState.Normal);
 
             this.View.AddSubview(greetingButton);
 
             var greetingLabel = new UILabel()
             {
-                Frame = new CGRect(StartX, 230, this.View.Bounds.Width, 50),
+                Frame = new CGRect(StartX, StartY+3*Height, this.View.Bounds.Width-2*StartX, Height),
                 Text = "Hello"
             };
             this.View.AddSubview(greetingLabel);
