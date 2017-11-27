2d3a3fdc4b981f0190eaaeae7492357482ef8939
Hrafn Loftsson <hrafn@kolibri.is>
2017-11-27 12:07:23 +0000

    Adds some views to HelloWorldController

--- Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/HelloWorldViewController.cs
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/HelloWorldViewController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using CoreGraphics;
 using UIKit;
 
 namespace HelloWorld.iOS
 {
     public partial class HelloWorldViewController : UIViewController
     {
+        private const double StartX = 20;
+
         public HelloWorldViewController() : base("HelloWorldViewController", null)
         {
         }
@@ -21,7 +23,42 @@ namespace HelloWorld.iOS
         {
             base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
+            this.View.BackgroundColor = UIColor.White;
+
+            var promptLabel = new UILabel()
+            {
+                Frame = new CGRect(StartX, 80, this.View.Bounds.Width, 50),
+                Text = "Enter your name: "
+            };
+
+            this.View.AddSubview(promptLabel);
+
+            var nameField = new UITextField()
+            {
+                Frame = new CGRect(StartX, 130, this.View.Bounds.Width, 50),
+                BorderStyle = UITextBorderStyle.RoundedRect
+            };
+
+            this.View.AddSubview(nameField);
+
+            var greetingButton = UIButton.FromType(UIButtonType.RoundedRect);
+            greetingButton.Frame = new CGRect(StartX, 180, this.View.Bounds.Width / 2, 50);
+            greetingButton.SetTitle("Greet me", UIControlState.Normal);
+
+            this.View.AddSubview(greetingButton);
+
+            var greetingLabel = new UILabel()
+            {
+                Frame = new CGRect(StartX, 230, this.View.Bounds.Width, 50),
+                Text = "Hello"
+            };
+            this.View.AddSubview(greetingLabel);
+
+            greetingButton.TouchUpInside += (sender, args) =>
+            {
+                nameField.ResignFirstResponder();
+                greetingLabel.Text = "Hello " + nameField.Text;
+            };
         }
     }
 }
