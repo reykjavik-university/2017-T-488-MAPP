f38fcc944db0df75b3f54617eef632e11534724d
Hrafn Loftsson <hrafn@kolibri.is>
2017-11-30 11:01:51 +0000

    Adds a PersonCollectionCell

---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/Views/PersonCollectionCell.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace HelloWorld.iOS.Views
+{
+    public class PersonCollectionCell : UICollectionViewCell
+    {
+        private readonly UIImageView _imageView;
+        private readonly UILabel _label;
+
+        public static readonly string CellId = "PersonCollectionCell";
+
+        [Export("initWithFrame:")]
+        public PersonCollectionCell(CGRect frame) : base(frame)
+        {
+            this._imageView = new UIImageView()
+            {
+                Frame = new CGRect(0,0,this.ContentView.Bounds.Width - 20, this.ContentView.Bounds.Height - 20),
+            };
+
+            this._label = new UILabel()
+            {
+                Frame = new CGRect(0, this.ContentView.Bounds.Height - 20, this.ContentView.Bounds.Width, 20),
+                Font = UIFont.FromName("AmericanTypewrite", 10f),
+                TextColor = UIColor.Red,
+                TextAlignment = UITextAlignment.Left,
+                BackgroundColor = UIColor.White
+            };
+
+            this.ContentView.AddSubviews(new UIView[] { this._imageView, this._label});
+        }
+
+        public void UpdateCell(Person person)
+        {
+            this._imageView.Image = UIImage.FromFile(person.ImageName);
+            this._label.Text = person.Name;
+        }
+    }
+}
