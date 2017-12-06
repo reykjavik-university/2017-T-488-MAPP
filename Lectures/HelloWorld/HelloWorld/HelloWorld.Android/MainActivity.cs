96aa18e22b0325a114f29103ba6349db2fefd953
Hrafn Loftsson <hrafn@kolibri.is>
2017-12-06 10:54:04 +0000

    Adds a NameInputFragment

--- Lectures/HelloWorld/HelloWorld/HelloWorld.Android/MainActivity.cs
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.Android/MainActivity.cs
@@ -26,32 +26,6 @@ namespace HelloWorld.Droid
 		    var toolbar = this.FindViewById<Toolbar>(Resource.Id.toolbar);
             this.SetActionBar(toolbar);
 		    this.ActionBar.Title = "My Toolbar";
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-		    var nameEditText = this.FindViewById<EditText>(Resource.Id.nameEditText);
-		    var greetingButton = this.FindViewById<Button> (Resource.Id.greetingButton);
-		    var greetingTextView = this.FindViewById<TextView>(Resource.Id.greetingTextView);
-		    var nameListButton = this.FindViewById<Button>(Resource.Id.nameListButton);
-
-		    greetingButton.Click += (object sender, EventArgs e) =>
-		    {
-		        var manager = (InputMethodManager)this.GetSystemService(InputMethodService);
-		        manager.HideSoftInputFromWindow(nameEditText.WindowToken, 0);
-		        greetingTextView.Text = "Hello " + nameEditText.Text;
-                People.Persons.Add(new Person()
-                {
-                    Name = nameEditText.Text
-                });
-		    };
-
-		    nameListButton.Click += (sender, args) =>
-		    {
-		        var intent = new Intent(this, typeof(NameListActivity));
-		        intent.PutExtra("personList", JsonConvert.SerializeObject(People.Persons));
-                this.StartActivity(intent);
-		    };
-
 		}
 	}
 }
---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.Android/NameInputFragment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Views.InputMethods;
+using Newtonsoft.Json;
+
+namespace HelloWorld.Droid
+{
+	[Activity (Label = "HelloWorld", Theme="@style/MyTheme")]
+	public class NameInputFragment : Fragment
+	{
+	    private readonly People _people;
+
+	    public NameInputFragment(People people)
+	    {
+	        this._people = people;
+	    }
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle bundle)
+		{
+		    var rootView = inflater.Inflate(Resource.Layout.NameInput, container, false);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+		    var nameEditText = rootView.FindViewById<EditText>(Resource.Id.nameEditText);
+		    var greetingButton = rootView.FindViewById<Button> (Resource.Id.greetingButton);
+		    var greetingTextView = rootView.FindViewById<TextView>(Resource.Id.greetingTextView);
+		    var nameListButton = rootView.FindViewById<Button>(Resource.Id.nameListButton);
+
+		    greetingButton.Click += (object sender, EventArgs e) =>
+		    {
+		        var manager = (InputMethodManager)this.Context.GetSystemService(Context.InputMethodService);
+		        manager.HideSoftInputFromWindow(nameEditText.WindowToken, 0);
+		        greetingTextView.Text = "Hello " + nameEditText.Text;
+                this._people.Persons.Add(new Person()
+                {
+                    Name = nameEditText.Text
+                });
+		    };
+
+		    nameListButton.Click += (sender, args) =>
+		    {
+		        var intent = new Intent(this.Context, typeof(NameListActivity));
+		        intent.PutExtra("personList", JsonConvert.SerializeObject(this._people.Persons));
+                this.StartActivity(intent);
+		    };
+
+		    return rootView;
+		}
+	}
+}
+
+
