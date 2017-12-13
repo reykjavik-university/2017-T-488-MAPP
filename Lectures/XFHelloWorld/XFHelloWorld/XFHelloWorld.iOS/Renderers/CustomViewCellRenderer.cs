dc97244c5a9bc4df3e3487cf503d9a215622f916
Hrafn Loftsson <hrafn@kolibri.is>
2017-12-13 11:13:15 +0000

    Adds a custom renderer for an iOS viewcell

---
+++ Lectures/XFHelloWorld/XFHelloWorld/XFHelloWorld.iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFHelloWorld.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(CustomViewCellRenderer))]
+
+namespace XFHelloWorld.iOS.Renderers
+{
+
+    public class CustomViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+            cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+
+            return cell;
+        }
+    }
+}
