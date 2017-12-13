974c77f09625ca4c9d9f232d6a73abcd075b7acd
Hrafn Loftsson <hrafn@kolibri.is>
2017-12-13 11:29:46 +0000

    Adds a user control button

---
+++ Lectures/XFHelloWorld/XFHelloWorld/XFHelloWorld/Pages/MyButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFHelloWorld.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyButton : Button
+    {
+        public MyButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
