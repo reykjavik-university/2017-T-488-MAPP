f137d165ec96bfb575c866983735a16c20a2e029
Hrafn Loftsson <hrafn@kolibri.is>
2017-12-13 10:52:04 +0000

    Adds a custom renderer for an entry in Android

---
+++ Lectures/XFHelloWorld/XFHelloWorld/XFHelloWorld.Android/Renderers/EntryWithBorderRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFHelloWorld.Droid.Renderers;
+using XFHelloWorld.Pages;
+
+[assembly: ExportRenderer(typeof(EntryWithBorder), typeof(EntryWithBorderRenderer))]
+
+namespace XFHelloWorld.Droid.Renderers
+{
+    public class EntryWithBorderRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control != null)
+            {
+                var nativeEditText = (global::Android.Widget.EditText) this.Control;
+                nativeEditText.SetBackgroundResource(Resource.Drawable.Box);
+            }
+        }
+    }
+}
